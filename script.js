20f9a7039db27c0db018050c64880d5a3bb6bddc
BRICE JOSHY <bricejoshy99@gmail.com>
2023-04-23 19:02:09 +0530

    html and css done

---
+++ script.js
@@ -0,0 +1,17 @@
+// initially experiance of player is 0
+let xp = 0;
+// health is max at start
+let health = 100;
+// starting gold
+let gold = 50;
+// starting weapon
+let currentWeapon = 0;
+// at the time of fight
+let fighting;
+// monsters health
+let monsterHealth;
+// the inventory can store multiple things so its an array
+// let inventory = ["stick","dagger","sword"];
+let inventory = ["stick"];
+
+
